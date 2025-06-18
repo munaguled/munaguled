6cc62321430bc3c6f93a3113d114b2959583b624
munaguled <munaguled7@gmail.com>
2025-06-18 15:34:58 +0100

    Update quiz.sh

--- quiz.sh
+++ quiz.sh
@@ -1,19 +1,19 @@
 echo -e "\e[1;4;33mWelcome to my quiz\e[0m"
-read -p " how old is muna  
-          A. 55
-          B. 68
-          C. 19
+read -p " Which city does Muna live in  
+          A. Norwich
+          B. Manchester
+          C. London
 "  x  
 read -p " what is muna surname
-        A.urmum
-        B.urdad
+        A.Smith
+        B.Johnson
         C.Guled
 " y 
 if [[ "$x" = "C" && "$y" = "C" ]]; then 
-echo  "  well done nigga you have scored two points " 
+echo  "  well done  you have scored two points " 
 elif [[ "$x" = "C" || "$y" = "C" ]]; then 
-echo " well done nigga you have scored one point " 
+echo " well done  you have scored one point " 
 else 
-echo " ur dumb ur score is 0 " 
+echo "  Your score is 0 " 
 fi
 
